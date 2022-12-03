ffa17841a9be577a60246fd242a35d62df385a15
Jane <90122530+jane424@users.noreply.github.com>
2022-12-03 16:55:26 +0900

    Create 3003.py

---
+++ 3003.py
@@ -0,0 +1,17 @@
+K, Q, R, Kn, B, P = input().split()
+
+K = int(K)
+Q = int(Q)
+R = int(R)
+Kn = int(Kn)
+B = int(B)
+P = int(P)
+
+a = 1 - K
+b = 1 - Q
+c = 2 - R
+d = 2 - Kn
+e = 2 - B
+f = 8 - P
+
+print(a, b, c, d, e, f)
