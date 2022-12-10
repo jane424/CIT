23b2fa4784b3f2cb9ee15f3f26efc57ef3a59247
Jane <90122530+jane424@users.noreply.github.com>
2022-12-10 15:01:50 +0900

    Create 2739.py

---
+++ 2739.py
@@ -0,0 +1,21 @@
+A = int(input())
+A = int(A)
+
+B = A * 2
+C = A * 3
+D = A * 4
+E = A * 5
+F = A * 6
+G = A * 7
+H = A * 8
+I = A * 9
+
+print(int(A), '* 1 =', int(A))
+print(int(A), '* 2 =', B)
+print(int(A), '* 3 =', C)
+print(int(A), '* 4 =', D)
+print(int(A), '* 5 =', E)
+print(int(A), '* 6 =', F)
+print(int(A), '* 7 =', G)
+print(int(A), '* 8 =', H)
+print(int(A), '* 9 =', I)
